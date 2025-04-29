895cb2b30d675863a503509d8ecf7f36925c0d0f
Kishore Kumar <kishorek1009@yahoo.com>
2025-04-29 18:12:02 +0530

    Update variable.tf
    error free code

--- variable.tf
+++ variable.tf
@@ -1,60 +1,60 @@
 variable "region" {
-  description = "AWS region for resouce"
-  type = string
-  default = "us-east-1"
-  }
-
-variable "vpc_cidr" {
-  description = "cidr for vpc"
-  type = string
-  default = "172.168.0.0/16"
+  description = "AWS region for resource"
+  type        = string
+  default     = "us-east-1"
 }
 
- variable "Availability zone" {
-  description = "one availability zone"
-  type = string
-  default = "us-east-1b"
+variable "vpc_cidr" {
+  description = "CIDR for VPC"
+  type        = string
+  default     = "172.168.0.0/16"
+}
+
+variable "availability_zone" {
+  description = "One availability zone"
+  type        = string
+  default     = "us-east-1b"
 }
 
 variable "pubsub_cidr" {
-  description = "cidr for pubsub"
-  type = string
-  default = "172.168.0.0/24"
+  description = "CIDR for public subnet"
+  type        = string
+  default     = "172.168.0.0/24"
 }
 
 variable "prisub_cidr" {
-  description = "cidr for prisub"
-  type = string
-  default = "172.168.1.0/24"
+  description = "CIDR for private subnet"
+  type        = string
+  default     = "172.168.1.0/24"
 }
 
 variable "default_ip" {
-  description = "Default ip address"
-  type = string
-  default = "0.0.0.0/0"
+  description = "Default IP address"
+  type        = string
+  default     = "0.0.0.0/0"
 }
 
 variable "AMI_id" {
- description = "AMI id for Ec2"
- type = string
- default = "ami-084568db4383264d4"
+  description = "AMI ID for EC2"
+  type        = string
+  default     = "ami-084568db4383264d4"
 }
 
 variable "instance_type" {
- description = "Instance type for Ec2"
- type = string
- default = "t2.micro"
+  description = "Instance type for EC2"
+  type        = string
+  default     = "t2.micro"
 }
 
 variable "user_data" {
- description = "User data for Ec2 instance"
- type = string
- default = "<<-EOF
-
-  sudo yum update -y
-  sudo yum install -y httpd
-  sudo systemctl start httpd
-  sudo systemctl enable httpd
-  echo "Hello world from Terraform!" > /var/www/html/index.html
-  EOF"
+  description = "User data for EC2 instance"
+  type        = string
+  default     = <<-EOF
+                #!/bin/bash
+                yum update -y
+                yum install -y httpd
+                systemctl start httpd
+                systemctl enable httpd
+                echo "Hello world from Terraform!" > /var/www/html/index.html
+                EOF
 }
